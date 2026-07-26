bd60a7701ca281972d948a64acd75a2673eba7b3
Hiro <wira.xp@gmail.com>
2026-07-26 23:16:35 +0700

    revert: remove batch release (toggle, form, server code)

--- app/release/page.tsx
+++ app/release/page.tsx
@@ -1,6 +1,6 @@
 import { requireAuth } from "@/lib/auth-utils";
 import { PageHeader } from "@/components/page-header";
-import { ReleaseSwitch } from "./release-switch";
+import { ReleaseForm } from "./release-form";
 import { AvailableItemsTable } from "./available-items-table";
 
 export default async function ReleasePage() {
@@ -9,7 +9,7 @@ export default async function ReleasePage() {
   return (
     <div className="max-w-4xl">
       <PageHeader title="Release Item" subtitle="Scan a serial to preview, then assign to a user" />
-      <ReleaseSwitch />
+      <ReleaseForm />
       <AvailableItemsTable />
     </div>
   );
